0118a1f95b97f7c8c7fe7062dba0092353d2e51f
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2011-01-08 17:15:35 +0100

    initial commit

---
+++ git-autopush.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+HOOKS_FOLDER=.git/hooks
+POST_COMMIT=$HOOKS_FOLDER/post-commit
+
+if [ -d $HOOKS_FOLDER ]; then
+    if [ -f $POST_COMMIT ]; then
+        echo "Post commit hook already exits, please 'git push' manually"
+        exit -1
+    fi
+    echo "git push" > $POST_COMMIT
+    chmod 755 $POST_COMMIT
+    exit 0
+else
+    echo "This command must be run in the root of a Git repository."
+    exit -1
+fi

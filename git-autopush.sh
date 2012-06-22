549374237ffd018fcb15bd06afc66867eeeafc83
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2012-06-22 14:23:44 +0200

    Reformatted stuff and fixed a few typos.

--- git-autopush.sh
+++ git-autopush.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-usage() 
+usage()
 {
 cat << EOF
 usage $0 [<options>] [-r <remoterepo>]
 
-will add a post-commit function in the git repo
-which will automaticly push to the default or remote repo
+Will add a post-commit function in the git repo which will automatically push
+to the default or remote repo.
 
 OPTIONS:
- -o	Overwrite post-commit
- -r 	Remote Repo
+ -o Overwrite any existing post-commit hook
+ -r Remote repo
 EOF
 }
 params="$@"
@@ -19,18 +19,18 @@ OVERWRITE=0
 
 while getopts  "hor:" OPTION
 do
-	case $OPTION in 
-		h)
-			usage
-			exit 1
-		;;
-		o)
-			OVERWRITE=1
-		;;
-		r)
-			REMOTEREPO="$OPTARG"
-		;;
-	esac
+  case $OPTION in
+    h)
+      usage
+      exit 1
+    ;;
+    o)
+      OVERWRITE=1
+    ;;
+    r)
+      REMOTEREPO="$OPTARG"
+    ;;
+  esac
 done
 shift $(( $OPTIND - 1 ))
 
@@ -38,20 +38,20 @@ HOOKS_FOLDER=.git/hooks
 POST_COMMIT=$HOOKS_FOLDER/post-commit
 
 if [ -d $HOOKS_FOLDER ]; then
-    if [ -f $POST_COMMIT ] && [ $OVERWRITE -eq 0 ]; then
-        echo "Post commit hook already exits, please add 'git push' manually in .git/hooks/post-commit"
-        exit 1
-    fi
-    if [ $OVERWRITE -eq 1 ]; then
-	mv $POST_COMMIT "$POST_COMMIT.bak"
-	echo "moved old hook to $POST_COMMIT.bak"
-    fi
-    echo "git push $REMOTEREPO" > $POST_COMMIT
-    chmod 755 $POST_COMMIT
-    REPOSITORY_BASENAME=$(basename "$PWD") 
-    echo "added auto commit to $REPOSITORY_BASENAME"
-    exit 0
-else
-    echo "This command must be run in the root of a Git repository."
+  if [ -f $POST_COMMIT ] && [ $OVERWRITE -eq 0 ]; then
+    echo "Post commit hook already exits, please add 'git push' manually in .git/hooks/post-commit"
     exit 1
+  fi
+  if [ $OVERWRITE -eq 1 ]; then
+    mv $POST_COMMIT "$POST_COMMIT.bak"
+    echo "moved old hook to $POST_COMMIT.bak"
+  fi
+  echo "git push $REMOTEREPO" > $POST_COMMIT
+  chmod 755 $POST_COMMIT
+  REPOSITORY_BASENAME=$(basename "$PWD")
+  echo "added auto commit to $REPOSITORY_BASENAME"
+  exit 0
+else
+  echo "This command must be run in the root of a Git repository."
+  exit 1
 fi
